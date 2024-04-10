887b341ee3f63c5d3bead67726f8c65b89b416f2
Nana McBrown <nanamcbrown@dhcp-10-250-80-229.harvard.edu>
2024-04-10 13:52:45 -0400

    first two vids done

--- source/App.tsx
+++ source/App.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'; // Ensure you have this CSS file for styles
 
 function App() {
+  const [activeVideoIndex, setActiveVideoIndex] = useState(0);
+
+  // Handles scroll event
+  const handleScroll = () => {
+    const scrollPosition = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+    // Simple logic to switch videos based on scroll position
+    // Adjust this logic based on your actual layout and how you want the transitions to happen
+    const newActiveIndex = Math.min(Math.floor(scrollPosition / windowHeight), videos.length - 1);
+    setActiveVideoIndex(newActiveIndex);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  // Define your videos here
+  const videos = [
+    "https://int.nyt.com/data/videotape/finished/2023/12/1702143468/stabilized_mountain-timelapse_colored_1-1254w.mp4",
+    "https://int.nyt.com/data/videotape/finished/2023/12/1702143786/grave-topper_colored-1254w.mp4", // Add the second video URL here
+    // Add more video URLs as needed
+  ];
+
   return (
     <div className="App">
-      <div className="video-background">
-        <video autoPlay muted loop style={{ position: 'fixed', right: 0, bottom: 0, minWidth: '100%', minHeight: '100%', zIndex: -1 }}>
-          <source src="path/to/your/video.mp4" type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
-      </div>
-      {/* Place any content you want on top of the video background here */}
-      <div className="content">
-        <h1>Welcome to Nana Graphic Run</h1>
-        {/* More content */}
+      {videos.map((src, index) => (
+        <div key={index} className={`video-background ${activeVideoIndex === index ? 'active' : ''}`}>
+          <video autoPlay muted loop style={{ position: 'fixed', right: 0, bottom: 0, minWidth: '100%', minHeight: '100%', objectFit: 'cover', zIndex: -1 }}>
+            <source src={src} type="video/mp4" />
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
+      ))}
+      {/* Content causing the page to scroll, thus enabling video transitions */}
+      <div style={{ height: `${videos.length * 100}vh` }}>
+        <div className="content">
+          <h1>Welcome to Nana's Graphic Test</h1>
+          {<p></p>}
+        </div>
       </div>
     </div>
   );
